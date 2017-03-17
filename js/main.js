6673f96f39dd17d37886153632bba9243b63888a
Grzegorz.Kruk <cr00q.pl@gmail.com>
2017-03-17 12:23:18 +0100

    change conception in header
    change conception in header

--- js/main.js
+++ js/main.js
@@ -4,52 +4,22 @@
     /*          Hamburger            */
     /*-------------------------------*/
     
-    
-    var trigger = $('.hamburger'),
-        overlay = $('.overlay'),
-        isClosed = false;
-
-    trigger.click(function () {
-        hamburger_cross();      
-    });
-
-    function hamburger_cross() {
-
-        if (isClosed == true) {          
-            overlay.hide();
-            trigger.removeClass('is-open');
-            trigger.addClass('is-closed');
-            isClosed = false;
-        } else {   
-            overlay.show();
-            trigger.removeClass('is-closed');
-            trigger.addClass('is-open');
-            isClosed = true;
-        }
-    }
-
-    $('[data-toggle="offcanvas"]').click(function () {
-        $('#wrapper').toggleClass('toggled');
+    $('.navbar-toggle').on('click', function () {
+        $(this).toggleClass('active');
     });
     
     /*-------------------------------*/
     /*           Preloade            */
     /*-------------------------------*/
-    
+        $('.navbar').hide(0).delay(2000).fadeIn(1000); //schowanie i pokazanie menu po załadowaniu preloadra
         $('#preloader-logo').fadeOut(2800); // zanikanie grafiki
         $('#preloader').delay(2000).fadeOut(3000); // zanikanie warstwy zakrywającej stronę
         $('body').delay(2800).css({'overflow':'visible'}); // dopóki #preloader jest widoczny nie ma możliwości przewijania strony
-    
     
     /*-------------------------------*/
     /*     Responsive Image-Map      */
     /*-------------------------------*/
     
-        $('img[usemap]').rwdImageMaps(); //resopnsywność pól mapy
-    
-        $('.map-garage').on('click', function(){
-            console.log('działa');
-        })
     
     
 }(jQuery);
